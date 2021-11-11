a7997a5feed5e33cdd70e0f4d264ad697c0c2f61
Igor Cantalice <igorcantalice@pop-os.localdomain>
2021-11-11 03:17:55 -0300

    atividade microsservicos

---
+++ microsservicos/mult/mult.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/mult', function(req, res){
+    var op1 = req.query.op1
+    var op2 = req.query.op2
+    var result = op1 * op2;
+    res.send(`Resultado = ${result}`)
+
+});
+
+app.listen(5000, function(){
+    console.log("Listening on port 5000")
+});
