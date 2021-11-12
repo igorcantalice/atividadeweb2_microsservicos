1501641e4d6871f2a57ed22e9f40e20c31e12e55
Igor Cantalice <igorcantalice@pop-os.localdomain>
2021-11-11 21:40:29 -0300

    Inserção de impressão no log /mult

--- microsservicos/mult/mult.js
+++ microsservicos/mult/mult.js
@@ -7,6 +7,7 @@ app.get('/mult', function(req, res){
     var op2 = req.query.op2
     var result = op1 * op2;
     res.send(`Resultado = ${result}`)
+    console.log(`Resultado = ${result}`)
 
 });
 
